2b6a6ec03adbc93d71288049623d09f6363f0ced
Alex Ficklin and Katie Fujihara <alex@>
2018-01-08 10:17:24 -0800

    Add BMI, Temperature and Gallon converter.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-};
+var weightPounds= prompt ("What is your weight? (pounds)");
+var heightFeet= prompt ("What is your height? (feet)");
+var weightKilo= weightPounds * 0.453592;
+var heightMeters= heightFeet * 0.3048;
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var bmi= weightKilo / heightMeters / heightMeters;
+alert(bmi);
 
-alert(add(number1, number2));
+if (bmi < 18.5) {
+alert("underweight");
+}
+
+else if ((18.5 < bmi) && (bmi < 24.9)) {
+alert("perf");
+}
+
+else {
+alert ("try losing a little weight")
+}
+
+
+var fahrenheit = prompt("Temperature in F");
+var celsius = ((parseInt(fahrenheit)-32)*(5/9));
+alert("The temperature in celsius is "+ celsius)
+
+var gallons = prompt("Gallons");
+var liters = ((parseInt(gallons)/.26417));
+alert("The amount of liters in your gallon is " + liters)
